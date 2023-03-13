e50b6d930a14588926799e1201fc1cc6c7c4b1b6
Jonathan Addington <jonathan.addington@jmaddington.com>
2023-03-13 19:00:35 -0400

    Modified to work with better directory structure

--- api/generatepythonapi.sh
+++ api/generatepythonapi.sh
@@ -1,5 +1,7 @@
 docker run --rm \
-  -v ${PWD}:/local openapitools/openapi-generator-cli generate \
+  -v ${PWD}:/local \
+  -v "${PWD}/../openapi.yml:/local/openapi.yml" \
+  openapitools/openapi-generator-cli generate \
   -i /local/openapi.yml \
   -g python \
   -o /local/api
