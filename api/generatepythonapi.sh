7d71fbdf6d8f59a00c841b81c17458fce8156ce7
Jonathan Addington <jonathan.addington@jmaddington.com>
2023-03-16 15:23:41 -0400

    Added OpenAPI Validation

--- api/generatepythonapi.sh
+++ api/generatepythonapi.sh
@@ -1,7 +1,22 @@
+echo "Validating itglue.yaml"
+
+valid=`swagger-cli validate itglue.yaml`
+
+# Check if the string contains "is valid"
+if [[ $valid == *"is valid"* ]]; then
+  echo "Valid OpenAPI YAML"
+else
+  echo "There is a problem with the OpenAPI YAML. Please fix it and try again."
+  exit 1
+fi
+
 docker run --rm \
   -v ${PWD}:/local \
-  -v "${PWD}/../itglue.yml:/local/itglue.yml" \
+  -v "${PWD}/../itglue.yaml:/local/itglue.yaml" \
   openapitools/openapi-generator-cli generate \
-  -i /local/itglue.yml \
+  -i /local/itglue.yaml \
+  --package-name itglue \
   -g python \
   -o /local/python/api
+
+exit 0
